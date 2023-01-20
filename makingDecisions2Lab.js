9288519f905e6c0e48e29d001603edc5d28a893a
Corbin Broadhead <poujmail@gmail.com>
2023-01-19 19:18:41 -0700

    Eight down...

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -109,6 +109,16 @@ console.log(someNum);
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
+if (myFavoriteNumbers.length < 7) {
+  console.log("There are not enough elements in this array.");
+} else if (myFavoriteNumbers.length > 7) {
+  console.log("There are more than 7 elements in this array.");
+  someNum = myFavoriteNumbers[6];
+} else {
+  console.log("There are exactly 7 elements in this array.");
+  someNum = myFavoriteNumbers[6];
+}
+
 //Code Here
 
 
@@ -119,6 +129,12 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+
+for (let i = 0; i < listOfNumbers.length; i++) {
+  if (listOfNumbers[i] % 3 === 0) {
+    console.log(`${listOfNumbers[i]} is divisible by 3.`);
+  }
+}
 
 //Code Here
 
